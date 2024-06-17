d8d5aed5f244e7e92171ee602edbb1daeb2195fc
Neve0 <nevenarcisista@gmail.com>
2024-06-17 15:04:54 -0300

    Mudança conexao com api

--- src/main/java/com/previdenciaprivada/emprestimos/services/BeneficiosConnectionImpl.java
+++ src/main/java/com/previdenciaprivada/emprestimos/services/BeneficiosConnectionImpl.java
@@ -13,18 +13,17 @@ import java.util.Objects;
 public class BeneficiosConnectionImpl implements BeneficiosConnection{
     @Override
     public double getSomaBeneficios(String CPF) {
-//        Dotenv dotenv = Dotenv.load();
-//        String URL =  "http://" + dotenv.get("SERVER_URL")  + "/api/v2.0/concessao/soma/" + CPF;
-//        RestTemplate connection = new RestTemplate();
+        Dotenv dotenv = Dotenv.load();
+        String URL =  "http://" + dotenv.get("SERVER_URL")  + "/api/v2.0/concessao/soma/" + CPF;
+        RestTemplate connection = new RestTemplate();
 
-//        try {
-//            ResponseEntity<String> response = connection.getForEntity(URL, String.class);
-//            return Double.parseDouble(Objects.requireNonNull(response.getBody().replaceAll("\\p{P}", "")));
-//        }
-//        // DEVE SER REFATORADO
-//        catch (RestClientException error) {
-//            return -1;
-//        }
-        return 20000;
+        try {
+            ResponseEntity<String> response = connection.getForEntity(URL, String.class);
+            return Double.parseDouble(Objects.requireNonNull(response.getBody().replaceAll("\\p{P}", "")));
+        }
+        // DEVE SER REFATORADO
+        catch (RestClientException error) {
+            return -1;
+        }
     }
 }
